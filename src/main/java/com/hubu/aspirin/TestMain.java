699c258138b856d8f791c34d471024459dc99271
alex <419725794@qq.com>
2020-10-11 23:10:44 +0800

    测试CI/DI

--- src/main/java/com/hubu/aspirin/TestMain.java
+++ src/main/java/com/hubu/aspirin/TestMain.java
@@ -3,9 +3,5 @@ package com.hubu.aspirin;
 
 public class TestMain {
     public static void main(String[] args) {
-        String a = "12345";
-        String[] abcs = a.split("abc");
-        System.out.println(abcs.length);
-        System.out.println(abcs[0]);
     }
 }
