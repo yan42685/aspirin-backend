53b37e09d0f635bb2fe75fcc39f4c8d3d780e387
alex <419725794@qq.com>
2020-11-19 22:48:48 +0800

    feat: 获取所有课程类型

--- src/main/java/com/hubu/aspirin/controller/BasicInformationController.java
+++ src/main/java/com/hubu/aspirin/controller/BasicInformationController.java
@@ -2,6 +2,7 @@ package com.hubu.aspirin.controller;
 
 
 import com.hubu.aspirin.common.JsonWrapper;
+import com.hubu.aspirin.enums.CourseTypeEnum;
 import com.hubu.aspirin.enums.RoleEnum;
 import com.hubu.aspirin.model.dto.*;
 import com.hubu.aspirin.service.*;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -80,5 +82,11 @@ public class BasicInformationController {
     @GetMapping("teacher")
     public JsonWrapper<TeacherDTO> getTeacherByNumber(String number) {
         return new JsonWrapper<>(teacherService.getDtoByNumber(number));
+    }
+
+    @ApiOperation("获取所有课程类型")
+    @GetMapping("course-type/list")
+    public JsonWrapper<List<CourseTypeEnum>> getAllCourseType() {
+        return new JsonWrapper<>(Arrays.asList(CourseTypeEnum.values()));
     }
 }
