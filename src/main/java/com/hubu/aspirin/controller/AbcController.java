637f5b4253346756ab75494b6ea05426af5f3061
alex <419725794@qq.com>
2020-11-21 11:03:51 +0800

    refactor

---
+++ src/main/java/com/hubu/aspirin/controller/AbcController.java
@@ -0,0 +1,12 @@
+package com.hubu.aspirin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AbcController {
+    @GetMapping
+    void abcdefghi(int hahah) {
+        return;
+    }
+}
