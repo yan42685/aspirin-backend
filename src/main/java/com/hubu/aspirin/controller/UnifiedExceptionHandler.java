c2279933ed60be8a2c97d5d56136bb4e7a63d85f
alex <419725794@qq.com>
2020-12-04 14:20:58 +0800

    refactor: 后端HTTP状态码尽量返回200

--- src/main/java/com/hubu/aspirin/controller/UnifiedExceptionHandler.java
+++ src/main/java/com/hubu/aspirin/controller/UnifiedExceptionHandler.java
@@ -7,11 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,7 +29,8 @@ public class UnifiedExceptionHandler {
     /**
      * 处理未知异常
      */
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  // 500状态码
+    // 建议全部返回200状态码，具体错误类型根据response里的数据来处理，这样方便前端写代码
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  // 500状态码
     @ExceptionHandler(Exception.class)
     public JsonWrapper<String> handleUnknownException(Exception e, HttpServletRequest request) {
         int errorCode = ExceptionEnum.UNKNOWN_EXCEPTION.getErrorCode();
@@ -44,7 +43,6 @@ public class UnifiedExceptionHandler {
     /**
      * 处理已知异常
      */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)  // 400状态码
     @ExceptionHandler(KnownException.class)
     public JsonWrapper<String> handleKnownException(KnownException e, HttpServletRequest request) {
         String stackTrack = Arrays.toString(e.getStackTrace());
@@ -55,7 +53,6 @@ public class UnifiedExceptionHandler {
     /**
      * 对于@Validated 普通参数(非 java bean)校验出错时抛出的异常
      */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
     public JsonWrapper<String> handleConstraintViolationException(ConstraintViolationException e) {
         int errorCode = ExceptionEnum.INVALID_PARAM.getErrorCode();
@@ -66,7 +63,6 @@ public class UnifiedExceptionHandler {
     /**
      * 对于@Validated 请求体绑定到java bean上失败时抛出的异常
      */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public JsonWrapper<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         int errorCode = ExceptionEnum.INVALID_PARAM.getErrorCode();
@@ -77,7 +73,6 @@ public class UnifiedExceptionHandler {
     /**
      * 对于@Valid 请求参数绑定到java bean上失败时抛出的异常
      */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public JsonWrapper<String> handleBindException(BindException e) {
         int errorCode = ExceptionEnum.INVALID_PARAM.getErrorCode();
@@ -88,8 +83,6 @@ public class UnifiedExceptionHandler {
     /**
      * 处理Shiro的未认证异常
      */
-    // NOTE: 返回401， 403会导致前端axios无法获取response (axios源码是这么写的)
-//    @ResponseStatus(HttpStatus.UNAUTHORIZED)  // 401状态码
     @ExceptionHandler(AuthenticationException.class)
     public JsonWrapper<String> handleUnAuthenticationException(Exception e, HttpServletRequest request) {
         int errorCode = ExceptionEnum.WRONG_CREDENTIALS.getErrorCode();
@@ -102,8 +95,6 @@ public class UnifiedExceptionHandler {
     /**
      * 处理Shiro的未授权异常
      */
-    // NOTE: 返回401， 403会导致前端axios无法获取response (axios源码是这么写的)
-//    @ResponseStatus(HttpStatus.UNAUTHORIZED)  // 401状态码
     @ExceptionHandler(AuthorizationException.class)
     public JsonWrapper<String> handleUnAuthorizationException(Exception e, HttpServletRequest request) {
         int errorCode = ExceptionEnum.NO_PERMISSION.getErrorCode();
