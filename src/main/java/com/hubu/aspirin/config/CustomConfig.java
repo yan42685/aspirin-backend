a188aa87d9c871bb65ea416095c6c22d9684e81f
alex <419725794@qq.com>
2020-12-08 22:21:04 +0800

    feat: 添加配置，开启管理员验证码验证

---
+++ src/main/java/com/hubu/aspirin/config/CustomConfig.java
@@ -0,0 +1,5 @@
+package com.hubu.aspirin.config;
+
+public class CustomConfig {
+    public static final boolean isDebug = false;
+}
--- src/main/java/com/hubu/aspirin/service/impl/UserServiceImpl.java
+++ src/main/java/com/hubu/aspirin/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hubu.aspirin.common.AspirinConstant;
 import com.hubu.aspirin.common.KnownException;
+import com.hubu.aspirin.config.CustomConfig;
 import com.hubu.aspirin.converter.BulletinConverter;
 import com.hubu.aspirin.converter.UserConverter;
 import com.hubu.aspirin.enums.ExceptionEnum;
@@ -20,6 +21,7 @@ import com.hubu.aspirin.service.TeacherService;
 import com.hubu.aspirin.service.UserService;
 import com.hubu.aspirin.util.QiniuUtils;
 import com.hubu.aspirin.util.UserUtils;
+import com.hubu.aspirin.util.VerificationCodeUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,11 +43,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDTO login(LoginTokenDTO dto) {
         // TODO: 上线后开放验证码检查
-//        RoleEnum role = UserUtils.getCurrentRole();
-//        // 对管理员检查验证码
-//        if (RoleEnum.ADMINISTRATOR.equals(role)) {
-//            VerificationCodeUtils.checkCode();
-//        }
+        if (!CustomConfig.isDebug) {
+            RoleEnum role = UserUtils.getCurrentRole();
+            // 对管理员检查验证码
+            if (RoleEnum.ADMINISTRATOR.equals(role)) {
+                VerificationCodeUtils.checkCode();
+            }
+        }
+
         Subject subject = SecurityUtils.getSubject();
         LoginToken token = new LoginToken(dto.getRole(), dto.getUsername(), dto.getPassword(), dto.getRememberMe());
         subject.login(token);
